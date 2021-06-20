6159d812c827751966f7fafe376efb19f85a08c6
Fredrik Dhami <fredrikdhami@hotmail.com>
2021-06-20 20:50:55 +0200

    Implementation of weekdays.

--- js/today.js
+++ js/today.js
@@ -2,17 +2,73 @@ function getTime() {
     return new Date().toLocaleTimeString();
 }
 
+function getWeekday() {
+    let weekdayNumber = new Date().getUTCDay();
+
+    switch(weekdayNumber) {
+        case 0:
+            return "Måndag";
+        case 1:
+            return "Tisdag";
+        case 2:
+            return "Onsdag";
+        case 3:
+            return "Torsdag";
+        case 4:
+            return "Fredag";
+        case 5:
+            return "Lördag";
+        case 6:
+            return "Söndag";
+    }
+}
+
 function getDate() {
-    return new Date().toLocaleDateString();
+    return String(new Date().getUTCDate());
+}
+
+function getMonth(){
+    let monthNumber = new Date().getUTCMonth();
+
+    switch(monthNumber) {
+        case 0:
+            return "januari";
+        case 1:
+            return "februari";
+        case 2:
+            return "mars";
+        case 3:
+            return "april";
+        case 4:
+            return "maj";
+        case 5:
+            return "juni";
+        case 6:
+            return "juli";
+        case 7:
+            return "augusti";
+        case 8:
+            return "september";
+        case 9:
+            return "oktober";
+        case 10:
+            return "november";
+        case 11:
+            return "december";
+    }
+}
+
+function getYear() {
+    return String(new Date().getUTCFullYear());
 }
 
 function displayTime() {
     let time = getTime();
-    appendSingleElement(time, ".active-day-event-content")
+    appendSingleElement(time, ".active-day-content")
 }
 
 function displayDate() {
-    let date = getDate();
+    let date = getWeekday() + ", " + getDate() + " " + getMonth() + " " + getYear();
     appendSingleElement(date, ".day-active")
 }
 
