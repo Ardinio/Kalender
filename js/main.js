f322b0ecddd33ed0f29d47c0c5db7df1febcfc6c
EnesMas <70940688+EnesMas@users.noreply.github.com>
2021-06-24 23:59:04 +0200

    Fixade positioneringen av todo räknaren..

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
-window.addEventListener('load', main);
+window.addEventListener("load", main);
 
 function main() {
-    runFunctionAtInterval(displayTime, 1000);
-    runFunctionAtInterval(displayDate, (86400000));
-    initTodos()
+  runFunctionAtInterval(displayTime, 1000);
+  runFunctionAtInterval(displayDate, 86400000);
+  initTodos();
 }
 
 function hasMobileWidth() {
-    return window.matchMedia('(max-width: 576px)').matches;
+  return window.matchMedia("(max-width: 576px)").matches;
 }
 
 const state = {
-    todos: [{
-        text: 'Ska jobba med skola',
-        date: new Date(2021, 06, 05)
-    }, {
-        text: 'Lorem ipsum dolor elit.',
-        date: new Date(2021, 06, 01)
-    }, {
-        text: 'Lorem ipsum adipisicing elit.',
-        date: new Date(2021, 06, 21)
-    }],
-    selectedDate: undefined
-}
+  todos: [
+    {
+      text: "Ska jobba med skola",
+      date: new Date(2021, 06, 05),
+    },
+    {
+      text: "Lorem ipsum dolor elit.",
+      date: new Date(2021, 06, 01),
+    },
+    {
+      text: "Lorem ipsum adipisicing elit.",
+      date: new Date(2021, 06, 21),
+    },
+  ],
+  selectedDate: undefined,
+};
 
 const isMobile = hasMobileWidth();
--- js/today.js
+++ js/today.js
@@ -1,86 +1,85 @@
 function getTime() {
-    return new Date().toLocaleTimeString();
+  return new Date().toLocaleTimeString();
 }
 
 function getWeekday() {
-    let weekdayNumber = new Date().getUTCDay();
+  let weekdayNumber = new Date().getUTCDay();
 
-    switch (weekdayNumber) {
-        case 0:
-            return "Söndag";
-        case 1:
-            return "Måndag";
-        case 2:
-            return "Tisdag";
-        case 3:
-            return "Onsdag";
-        case 4:
-            return "Torsdag";
-        case 5:
-            return "Fredag";
-        case 6:
-            return "Lördag";
-    }
+  switch (weekdayNumber) {
+    case 0:
+      return "Söndag";
+    case 1:
+      return "Måndag";
+    case 2:
+      return "Tisdag";
+    case 3:
+      return "Onsdag";
+    case 4:
+      return "Torsdag";
+    case 5:
+      return "Fredag";
+    case 6:
+      return "Lördag";
+  }
 }
 
 function getDate() {
-    return String(new Date().getUTCDate());
+  return String(new Date().getUTCDate());
 }
 
 function getMonth() {
-    let monthNumber = new Date().getUTCMonth();
+  let monthNumber = new Date().getUTCMonth();
 
-    switch (monthNumber) {
-        case 0:
-            return "januari";
-        case 1:
-            return "februari";
-        case 2:
-            return "mars";
-        case 3:
-            return "april";
-        case 4:
-            return "maj";
-        case 5:
-            return "juni";
-        case 6:
-            return "juli";
-        case 7:
-            return "augusti";
-        case 8:
-            return "september";
-        case 9:
-            return "oktober";
-        case 10:
-            return "november";
-        case 11:
-            return "december";
-    }
+  switch (monthNumber) {
+    case 0:
+      return "januari";
+    case 1:
+      return "februari";
+    case 2:
+      return "mars";
+    case 3:
+      return "april";
+    case 4:
+      return "maj";
+    case 5:
+      return "juni";
+    case 6:
+      return "juli";
+    case 7:
+      return "augusti";
+    case 8:
+      return "september";
+    case 9:
+      return "oktober";
+    case 10:
+      return "november";
+    case 11:
+      return "december";
+  }
 }
 
 function getYear() {
-    return String(new Date().getUTCFullYear());
+  return String(new Date().getUTCFullYear());
 }
 
 function displayTime() {
-    let time = getTime();
-    appendSingleElement(time, ".active-day-content-clock")
+  let time = getTime();
+  appendSingleElement(time, ".active-day-content-clock");
 }
 
 function displayDate() {
-    let date = getWeekday() + ", " + getDate() + " " + getMonth() + " " + getYear();
-    appendSingleElement(date, ".active-day-content-date")
+  let date =
+    getWeekday() + ", " + getDate() + " " + getMonth() + " " + getYear();
+  appendSingleElement(date, ".active-day-content-date");
 }
 
-
 function runFunctionAtInterval(functionName, milliseconds) {
-    functionName();
-    setInterval(functionName, milliseconds);
+  functionName();
+  setInterval(functionName, milliseconds);
 }
 
 function appendSingleElement(elementToAppend, parentSelector) {
-
-    let parentElement = document.querySelector(parentSelector);
-    parentElement.innerHTML = "";
-    parentElement.append(elementToAppend);
+  let parentElement = document.querySelector(parentSelector);
+  parentElement.innerHTML = "";
+  parentElement.append(elementToAppend);
 }
